1747d105b8d8e79a0864950f388ef33a410722c8
SnowCracker <1639722054@qq.com>
2026-03-31 19:03:27 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,11 @@
+// data.js
 let planes = [];
+
+// 从 localStorage 读取数据
+if (localStorage.planeGallery) {
+    try {
+        planes = JSON.parse(localStorage.planeGallery);
+    } catch (e) {
+        planes = [];
+    }
+}

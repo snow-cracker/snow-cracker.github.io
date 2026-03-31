f18e1c4990d06c11e2c3e37c1976b05c6d6645be
SnowCracker <1639722054@qq.com>
2026-03-31 21:30:37 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,43 +1,34 @@
-// data.js
 let planes = [];
 
-// IndexedDB 本地存储
 const DB_NAME = 'planeGalleryDB';
 const DB_STORE = 'planes';
 let db;
 
 function initDB(callback){
     const request = indexedDB.open(DB_NAME,1);
-    request.onupgradeneeded = function(e){
+    request.onupgradeneeded = e => {
         db = e.target.result;
         if(!db.objectStoreNames.contains(DB_STORE)){
             db.createObjectStore(DB_STORE,{keyPath:'id',autoIncrement:true});
         }
     };
-    request.onsuccess = function(e){
-        db = e.target.result;
-        loadPlanes(callback);
-    };
-    request.onerror = function(){ alert('数据库初始化失败'); }
+    request.onsuccess = e => { db = e.target.result; loadPlanes(callback); };
+    request.onerror = e => { alert('数据库初始化失败'); };
 }
 
 function savePlanes(){
     if(!db) return;
     const tx = db.transaction(DB_STORE,'readwrite');
     const store = tx.objectStore(DB_STORE);
-    store.clear().onsuccess = function(){
+    store.clear().onsuccess = () => {
         planes.forEach(p=>store.add(p));
-    };
+    }
 }
 
-// 拉取本地数据
 function loadPlanes(callback){
     if(!db) return;
     const tx = db.transaction(DB_STORE,'readonly');
     const store = tx.objectStore(DB_STORE);
     const req = store.getAll();
-    req.onsuccess = function(){
-        planes = req.result;
-        callback && callback();
-    };
+    req.onsuccess = () => { planes = req.result; callback && callback(); };
 }

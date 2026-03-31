3880d72fcd345d318399c10a199beec20385a632
SnowCracker <1639722054@qq.com>
2026-03-31 21:25:30 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,23 @@
 // data.js
 let planes = [];
 
+// IndexedDB 本地存储
 const DB_NAME = 'planeGalleryDB';
 const DB_STORE = 'planes';
 let db;
 
 function initDB(callback){
     const request = indexedDB.open(DB_NAME,1);
-    request.onupgradeneeded = function(event){
-        db = event.target.result;
+    request.onupgradeneeded = function(e){
+        db = e.target.result;
         if(!db.objectStoreNames.contains(DB_STORE)){
             db.createObjectStore(DB_STORE,{keyPath:'id',autoIncrement:true});
         }
-    }
-    request.onsuccess = function(event){
-        db = event.target.result;
+    };
+    request.onsuccess = function(e){
+        db = e.target.result;
         loadPlanes(callback);
-    }
+    };
     request.onerror = function(){ alert('数据库初始化失败'); }
 }
 
@@ -26,16 +27,17 @@ function savePlanes(){
     const store = tx.objectStore(DB_STORE);
     store.clear().onsuccess = function(){
         planes.forEach(p=>store.add(p));
-    }
+    };
 }
 
+// 拉取本地数据
 function loadPlanes(callback){
     if(!db) return;
     const tx = db.transaction(DB_STORE,'readonly');
     const store = tx.objectStore(DB_STORE);
     const req = store.getAll();
-    req.onsuccess = function(){ 
+    req.onsuccess = function(){
         planes = req.result;
         callback && callback();
-    }
+    };
 }
